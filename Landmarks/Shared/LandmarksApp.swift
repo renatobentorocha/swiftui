53591d48c4188db8513f414095a92d739ef56275
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-06-06 22:07:23 -0300

    Creating and combining views

---
+++ Landmarks/Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Renato  Rocha on 06/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
