7ca36fc37e4134d96f41fd0baec7b1f00e8c6e8e
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-06-07 12:15:41 -0300

    Create the Row View

---
+++ Landmarks/Shared/Views/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  Landmarks (iOS)
+//
+//  Created by Renato  Rocha on 07/06/22.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landMark: Landmark
+    
+    var body: some View {
+        HStack {
+            landMark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landMark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landMark: landmarks[0])
+    }
+}
