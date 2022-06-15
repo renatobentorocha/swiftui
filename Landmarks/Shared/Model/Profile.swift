b41588f686c54ee1116ae6f6feb7a6ab79ee9a57
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-06-15 12:13:23 -0300

    Display a User Profile

---
+++ Landmarks/Shared/Model/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  Landmarks (iOS)
+//
+//  Created by Renato  Rocha on 15/06/22.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications: Bool = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+    
+}
--- Landmarks/Shared/Views/Categories/CategoryHome.swift
+++ Landmarks/Shared/Views/Categories/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
@@ -32,7 +33,19 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
         
     }
