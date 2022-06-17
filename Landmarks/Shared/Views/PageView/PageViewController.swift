f9a5892246f0829f754794e3e3b6199c95b6f279
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-06-16 22:16:34 -0300

    Create the View Controller’s Data Source

--- Landmarks/Shared/Views/PageView/PageViewController.swift
+++ Landmarks/Shared/Views/PageView/PageViewController.swift
@@ -13,17 +13,57 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     
     func makeUIViewController(context: Context) -> UIPageViewController {
-        UIPageViewController(
+        let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
-            navigationOrientation: .horizontal
-        )
+            navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
+
+        return pageViewController
     }
     
     func updateUIViewController(_ uiViewController: UIPageViewController, context: Context) {
         uiViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])],
+            [context.coordinator.controllers[0]],
             direction: .forward,
             animated: true
         )
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index == 0 {
+                return controllers.last
+            }
+            
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            
+            return controllers[index + 1]
+        }
+        
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
     }
 }
