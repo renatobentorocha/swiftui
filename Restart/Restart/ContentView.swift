e1fed00b68eb7617a18726096edcdfe1ff784a5b
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-07-01 22:09:32 -0300

    Restart - Add assets

---
+++ Restart/Restart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Renato  Rocha on 01/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Renato  Rocha on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
