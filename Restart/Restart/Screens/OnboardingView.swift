8246a9ecd006dfa8888a232db90796a1a2cfa6a6
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-07-04 20:58:36 -0300

    Restart - onboarding view

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -11,16 +11,46 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
-            Button {
-                isOnboardingViewActive.toggle()
-            } label: {
-                Text("START")
-            }
-
-        }
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack(spacing: 20) {
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    Text("""
+                    It's is not how much we give but
+                    how much love we put into given.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: Header
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: Center
+                
+                Spacer()
+            } // VStack
+        } // ZStack
     }
 }
 
