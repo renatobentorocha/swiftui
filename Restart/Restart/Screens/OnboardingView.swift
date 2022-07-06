b9d45719809345797aafe0dfc5766e4b95fcc225
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-07-05 23:30:03 -0300

    Restart - onboarding design

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -49,6 +49,47 @@ struct OnboardingView: View {
                 } //: Center
                 
                 Spacer()
+                
+                ZStack {
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(Color.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        Spacer()
+                    }
+                } //: Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
             } // VStack
         } // ZStack
     }
