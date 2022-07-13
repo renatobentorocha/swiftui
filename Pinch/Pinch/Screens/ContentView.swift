9bd209b34b689a10370cf84bbe7774311f56184b
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-07-13 11:59:16 -0300

    Pinch - Double tap gesture

--- Pinch/Pinch/Screens/ContentView.swift
+++ Pinch/Pinch/Screens/ContentView.swift
@@ -8,10 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .foregroundColor(.primary)
+        NavigationView {
+            ZStack {
+                // MARK: Page image
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .onAppear {
+                        isAnimating.toggle()
+                    }
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        withAnimation(.spring()) {
+                            imageScale = imageScale == 1 ? 5 : 1
+                        }
+                    }
+            }
+            .navigationTitle("Pinch && Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
